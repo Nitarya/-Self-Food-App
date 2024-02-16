d9cdef663bd4b8749356fa924f1c71da349c2801
Nitya <niittss27@gmail.com>
2024-02-16 20:49:31 +0530

    food app

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -6,11 +6,21 @@ import Input from "./UI/Input";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
 
-currencyFormatter;
+const requestConfig = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  }
+};
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
+
+  const { data, isLoading, error, sendRequest } = useHttp(
+    "http//localhost:3000/orders",
+    requestConfig
+  );
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -26,6 +36,14 @@ export default function Checkout() {
 
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries()); // { email: test@example.com }
+
+
+sendRequest({
+    order: {
+      items: cartCtx.items,
+      customer: customerData,
+    },
+  })
 
     fetch("http://localhost:3000/orders", {
       method: "POST",
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,7 @@
 
 import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
+import Error from "./Error";
 
 const requestConfig = {}
 
@@ -12,7 +13,11 @@ const {
 } = useHttp('http://localhost:3000/meals', requestConfig, [])
   
 if (isLoading) {
-    return <p>Fetching meals...</p>
+    return <p className="center">Fetching meals...</p>
+}
+
+if (error) {
+    return <Error title="Failed to fetch meals" message={error}/>
 }
 
   return (
