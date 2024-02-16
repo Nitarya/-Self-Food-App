c0d64afccbaa2ce19c9d4737d528433053e6c104
Nitya <niittss27@gmail.com>
2024-02-16 15:08:33 +0530

    Form page added to checkout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,7 +4,7 @@ import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/formatting.js";
 import Button from "./UI/Button.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
-import CartItem from "./UI/CartItem.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -22,17 +22,23 @@ export default function Cart() {
     userProgressCtx.hideCart();
   }
 
+  function handleGoToCheckout() {
+    userProgressCtx.showCheckout()
+  }
+
   return (
-    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+    <Modal className="cart" open={userProgressCtx.progress === "cart"} 
+    onClose={ userProgressCtx.progress === 'cart' ? handleCloseCart : null}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <CartItem key={item.id} 
-          name={item.name}
-          quantity={item.quantity}
-          price={item.price}
-          onDecrease={() => cartCtx.removeItem(item.id)}
-          onIncrease={() => cartCtx.addItem(item)}
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+            onIncrease={() => cartCtx.addItem(item)}
           />
         ))}
       </ul>
@@ -41,7 +47,10 @@ export default function Cart() {
         <Button textOnly onClick={handleCloseCart}>
           Close
         </Button>
-        <Button onClick={handleCloseCart}>Go to Checkout</Button>
+
+        {cartCtx.items.length > 0 && (
+          <Button onClick={handleGoToCheckout}>Go to Checkout</Button>
+        )}
       </p>
     </Modal>
   );
